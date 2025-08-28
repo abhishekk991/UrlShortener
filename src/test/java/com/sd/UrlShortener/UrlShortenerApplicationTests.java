cb629651417a0d5323e695b4e0f63a91bd1aab29
abhishekk991 <41034347+abhishekk991@users.noreply.github.com>
2025-08-28 11:40:25 -0700

    First commit - Adding project structure

---
+++ src/test/java/com/sd/UrlShortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sd.UrlShortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
